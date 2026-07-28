5cd0120072f2115fbbffb041be1d03e45c3ee660
kris1027 <kris1027.dev@gmail.com>
2026-07-28 16:41:35 +0200

    feat: add tabs to the home page to switch between shows and movies

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import type { JSX } from 'react';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { trendingMovies, trendingShows } from '@/lib/tmdb';
 
 const HomePage = async (): Promise<JSX.Element> => {
@@ -9,46 +10,60 @@ const HomePage = async (): Promise<JSX.Element> => {
 
   return (
     <main className='p-4 flex flex-col items-center gap-4'>
-      <h2 className='text-xl bold font-bold'>
-        Popularne seriale w tym tygodniu:
-      </h2>
-      <ul className='grid grid-cols-1 gap-4'>
-        {shows.map((show) => (
-          <li key={show.id}>
-            <Image
-              src={`${process.env.TMDB_POSTER_PATH}${show.poster_path}`}
-              width={342}
-              height={513}
-              alt={show.name}
-              loading='eager'
-            />
-            <div className='flex justify-between p-1'>
-              <h2 className='font-semibold'>{show.name}</h2>
-              <p className='font-semibold'>{show.vote_average.toFixed(1)}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-      <h2 className='text-xl bold font-bold'>
-        Popularne filmy w tym tygodniu:
-      </h2>
-      <ul className='grid grid-cols-1 gap-4'>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Image
-              src={`${process.env.TMDB_POSTER_PATH}${movie.poster_path}`}
-              width={342}
-              height={513}
-              alt={movie.title}
-              loading='eager'
-            />
-            <div className='flex justify-between p-1'>
-              <h2 className='font-semibold'>{movie.title}</h2>
-              <p className='font-semibold'>{movie.vote_average.toFixed(1)}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Tabs className='flex items-center'>
+        <TabsList>
+          <TabsTrigger value={shows}>Seriale</TabsTrigger>
+          <TabsTrigger value={movies}>Filmy</TabsTrigger>
+        </TabsList>
+        <TabsContent value={shows}>
+          <h2 className='text-xl bold font-bold'>
+            Popularne seriale w tym tygodniu:
+          </h2>
+          <ul className='grid grid-cols-1 gap-4'>
+            {shows.map((show) => (
+              <li key={show.id}>
+                <Image
+                  src={`${process.env.TMDB_POSTER_PATH}${show.poster_path}`}
+                  width={342}
+                  height={513}
+                  alt={show.name}
+                  loading='eager'
+                />
+                <div className='flex justify-between p-1'>
+                  <h2 className='font-semibold'>{show.name}</h2>
+                  <p className='font-semibold'>
+                    {show.vote_average.toFixed(1)}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </TabsContent>
+        <TabsContent value={movies}>
+          <h2 className='text-xl bold font-bold'>
+            Popularne filmy w tym tygodniu:
+          </h2>
+          <ul className='grid grid-cols-1 gap-4'>
+            {movies.map((movie) => (
+              <li key={movie.id}>
+                <Image
+                  src={`${process.env.TMDB_POSTER_PATH}${movie.poster_path}`}
+                  width={342}
+                  height={513}
+                  alt={movie.title}
+                  loading='eager'
+                />
+                <div className='flex justify-between p-1'>
+                  <h2 className='font-semibold'>{movie.title}</h2>
+                  <p className='font-semibold'>
+                    {movie.vote_average.toFixed(1)}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 };
